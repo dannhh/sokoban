421321f29ef45ef575cd03b36f12a6051ab0b166
HODA <hongdancuibap@gmail.com>
2021-09-21 13:15:57 +0700

    update a-star

--- a-star-heuristic.py
+++ a-star-heuristic.py
@@ -1,3 +1,4 @@
+from os import PRIO_USER
 import sys
 import numpy as np
 from collections import deque
@@ -28,10 +29,10 @@ def heuristic(box_ls,storage_ls,path,point_ls):
     
     return dis_value
 
-
-# function for reading a file
-def print_char():
-    f = open("/home/daneiii/Documents/AI-sokoban/test-case-one.txt", 'r')
+def print_char(filename):
+    f = open(filename, 'r')
+# def print_char():
+#     f = open("/home/daneiii/Documents/AI-sokoban/sokoban/test-case-one.txt", 'r')
     i = 0
     j = 0
     new = []
@@ -73,10 +74,20 @@ def print_char():
         else:
             break
 
-print_char()
-visited=[]
+
+if len(sys.argv)<2:
+	print("please provide textfile name as system argument \n python bfs.py <filename>")
+	exit(0)
+print_char(sys.argv[1])
+# print_char()
+visited={}
 queue=[]
 
+if len(robot) == 0 or len(box) == 0 or len(storage) == 0 or len(wall) == 0:
+	print("please provide the textfile in write format Walls :O \n storage : S \n box : B \n robot : R \n box on storage : . \n robot on storage : $ \n should include walls,storage,box,robot")
+	exit(0)
+
+storage.sort()
 # function for movement of a box
 
 def move(point,dir,path,temp_box_list):
@@ -95,30 +106,25 @@ def move(point,dir,path,temp_box_list):
             temp_box=[x + y for x, y in zip(point, directions[dir])]
             if temp_box not in box_list and wall[temp_box[0]][temp_box[1]] == 0:
                 box_list[ind]=[x + y for x, y in zip(point, directions[dir])]
+                box_list.sort()
+
                 temp_append.append(point)
                 for i in box_list:
                     temp_append.append(i)
-                if temp_append not in visited:
-                    counter=0
-                    popped_robot=temp_append[0]
-                    for k in visited:
-                        k_temp_visited=k[:]
-                        
-                        k_robot=k_temp_visited.pop(0)
-                        temporary=[]
-                        if k_robot==popped_robot:
-                            count_list=0
-                            temporary=temp_append[1:]
-                            for m in k_temp_visited:
-                                if m in temporary:
-                                    count_list=count_list+1
-                            if count_list==len(temporary):
-                                counter=counter+1
-                    if counter==0:
-                        temp_append.append(cur_path)
-                        dis_append=heuristic(box_list,storage,cur_path,point)
-                        temp_append.append(dis_append)
-                        queue.append(temp_append)
+                
+                idx = point[0]*10 + point[1]
+                counter = 0
+                if idx in visited:
+                    for k in visited[idx]:
+                        if(k == temp_append):
+                            counter = counter + 1
+
+                if counter==0:
+                    temp_append.append(cur_path)
+                    dis_append=heuristic(box_list,storage,cur_path,point)
+                    temp_append.append(dis_append)
+                    queue.append(temp_append)
+
                 if set(map(tuple,box_list))==set(map(tuple,storage)):
                     stop = timeit.default_timer()
                     total_time=stop-start_time
@@ -131,29 +137,23 @@ def move(point,dir,path,temp_box_list):
                     exit()
         else:
             temp_append.append(point)
+            box_list.sort()
             for i in box_list:
                 temp_append.append(i)
-            if temp_append not in visited:
-                counter=0
-                popped_robot=temp_append[0]
-                for k in visited:
-                    k_temp_visited=k[:]
-                    count_list=0
-                    k_robot=k_temp_visited.pop(0)
-                    temporary=[]
-                    if k_robot==popped_robot:
-                        temporary=temp_append[1:]
-                        for m in k_temp_visited:
-                            if m in temporary:
-                                count_list=count_list+1
-                        if count_list==len(temporary):
-                            counter=counter+1
-                if counter==0:
-                    dis_append=heuristic(box_list,storage,cur_path,point)
-                    temp_append.append(cur_path)
-                    temp_append.append(dis_append)
-                    
-                    queue.append(temp_append)
+            
+            idx = point[0]*10 + point[1]
+            counter = 0
+            if idx in visited:
+                for k in visited[idx]:
+                    if(k == temp_append):
+                        counter = counter + 1
+
+            if counter==0:
+                temp_append.append(cur_path)
+                dis_append=heuristic(box_list,storage,cur_path,point)
+                temp_append.append(dis_append)
+                queue.append(temp_append)
+
             if set(map(tuple,box_list))==set(map(tuple,storage)):
                 stop = timeit.default_timer()
                 total_time=stop-start_time
@@ -173,17 +173,19 @@ dir_W='W'
 
 # function for A* algorithm
 
-def Astar_heuristic():
-    start_time = timeit.default_timer()
+start_time = timeit.default_timer()
+def Astar_heuristic():    
     # temp_queue contains current queue, form: [[]]
     temp_queue = []
     # store current robot's position, form: []
     initial = robot[0]
     temp_queue.append(initial) # form: [robot[0]]: [[]]
     # box form: [[]]
+    box.sort()
     for i in box:
         temp_queue.append(i) # append each box into temp_queue
     path=[]
+    temp_queue
     temp_queue.append(path)
     temp_queue.append(0)
     queue.append(temp_queue) # queue form: [[[]]] = [0:temp_queue]
@@ -200,8 +202,21 @@ def Astar_heuristic():
         robot_position = robot_position_list[0]
         # visited_adding: queue \ {path, 0}
         visited_adding = robot_position_list[:-2]
-        if visited_adding not in visited:
-            visited.append(visited_adding)		
+
+        idx = robot_position[0]*10 + robot_position[1] 
+        if idx in visited:     
+            counter = 0   
+            for pos in visited[idx]:
+                if(visited_adding == pos):
+                    counter = 1
+                    break
+                    
+            if(counter == 0):
+                visited[idx].append(visited_adding)            
+        else:
+            visited[idx] = []
+            visited[idx].append(visited_adding)            
+
         # path only
         temp_path = robot_position_list[-2][:]
         # box only [[]]
@@ -219,6 +234,8 @@ def Astar_heuristic():
         move(W,dir_W,temp_path,temp_box_list)
         
         count = count + 1
+        if(count == 44):
+            count = count
         if not queue:
             print("solution not found")
             exit()
