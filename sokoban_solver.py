c5e02937b9f282d65fcc18f50deb12501940e683
nhthuong2208 <nguyenhoaithuong5690@gmail.com>
2021-10-07 22:58:52 +0700

    Add board game to show visualization

--- sokoban_solver.py
+++ sokoban_solver.py
@@ -4,7 +4,9 @@ import heapq
 import psutil
 import os
 from collections import deque
-import gc
+import pygame
+from Environment import Environment
+from Level import Level
 
 #--------------------------------INITIAL--------------------------------#
 # use to take memory used
@@ -90,10 +92,19 @@ def write_file(filename, cur_path, total_time, total_step, space_taken, total_st
 
 # main function to run
 def run(fileread, filewrite):
+    global level_set
+    if fileread == "MiniCosmos.txt":
+        level_set = "minicosmos"
+    else:
+        level_set = "microcosmos"
     fulltest, list_level = read_file(fileread)
     count = 0
     while True:
-        global robot, walls, box, storage, visited_Moves, queue, dequeue, check, width
+        # array to take step to run in game
+        A_star_path = []
+        bfs_path = []
+
+        global robot, walls, box, storage, visited_Moves, queue, dequeue, check, width, current_level
         print("Choose testcase to run (1 -> 40), enter -1 to quit: ")
         level_choose = input()
 
@@ -112,6 +123,7 @@ def run(fileread, filewrite):
         if algorithm_choose == "1":
             start_time = time.time()
             cur_path, total_time, total_step, space_taken, total_state = bfs()
+            bfs_path = cur_path
             if cur_path == []:
                 file = open(filewrite, 'a')
                 file.write("Testcase is fail\n")
@@ -122,6 +134,7 @@ def run(fileread, filewrite):
         elif algorithm_choose == "2":
             start_time = time.time()
             cur_path, total_time, total_step, space_taken, total_state = A_star_heuristic()
+            A_star_path = cur_path
             if cur_path == []:
                 file = open(filewrite, 'a')
                 file.write("Testcase is fail\n")
@@ -133,6 +146,7 @@ def run(fileread, filewrite):
             """###############-----------RUN BFS-----------###############"""
             start_time = time.time()
             cur_path, total_time, total_step, space_taken, total_state = bfs()
+            bfs_path = cur_path
             if cur_path == []:
                 file = open(filewrite, 'a')
                 file.write("Testcase is fail\n")
@@ -146,6 +160,7 @@ def run(fileread, filewrite):
             check = False           #------------run BFS-----------------#
             start_time = time.time()
             cur_path, total_time, total_step, space_taken, total_state = A_star_heuristic()
+            A_star_path = cur_path
             if cur_path == []:
                 file = open(filewrite, 'a')
                 file.write("Testcase is fail\n")
@@ -167,6 +182,44 @@ def run(fileread, filewrite):
         queue = []
         check = False
         dequeue = deque()
+
+        current_level = int(level_choose)
+        runtime = False    # check the game completed or not
+        # Initialize Level
+        initLevel(level_set,current_level)
+        while runtime == False:
+            for event in pygame.event.get():
+                if runtime == True:
+                    break
+                #if event.type == pygame.KEYDOWN:
+                if bfs_path:
+                    for i in bfs_path:
+                        if i == "U":
+                            movePlayer("U", myLevel)
+                        elif i == "D":
+                            movePlayer("D", myLevel)
+                        elif i == "L":
+                            movePlayer("L", myLevel)
+                        else:
+                            movePlayer("R", myLevel)
+                        time.sleep(0.5)
+                initLevel(level_set, current_level)
+                if A_star_path:
+                    for i in A_star_path:
+                        if i == "U":
+                            movePlayer("U", myLevel)
+                        elif i == "D":
+                            movePlayer("D", myLevel)
+                        elif i == "L":
+                            movePlayer("L", myLevel)
+                        else:
+                            movePlayer("R", myLevel)
+                        time.sleep(0.5)
+                runtime = True
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    #sys.exit()
+
     
     return count
 
@@ -740,6 +793,376 @@ def bfs():
             if check == True:
                 break
     return cur_path, total_time, total_step, space_taken, count
+
+# game
+def drawLevel(matrix_to_draw):
+	
+	# Load level images
+	wall = pygame.image.load(myEnvironment.getPath() + '/themes/' + theme + '/images/wall.png').convert()
+	box = pygame.image.load(myEnvironment.getPath() + '/themes/' + theme + '/images/box.png').convert()
+	box_on_target =  pygame.image.load(myEnvironment.getPath() + '/themes/' + theme + '/images/box_on_target.png').convert()
+	space = pygame.image.load(myEnvironment.getPath() + '/themes/' + theme + '/images/space.png').convert()
+	target = pygame.image.load(myEnvironment.getPath() + '/themes/' + theme + '/images/target.png').convert()
+	player = pygame.image.load(myEnvironment.getPath() + '/themes/' + theme + '/images/player.png').convert()
+	
+	# If horizontal or vertical resolution is not enough to fit the level images then resize images
+	if myLevel.getSize()[0] > myEnvironment.size[0] / 36 or myLevel.getSize()[1] > myEnvironment.size[1] / 36:
+		
+		# If level's x size > level's y size then resize according to x axis
+		if myLevel.getSize()[0] / myLevel.getSize()[1] >= 1:
+			new_image_size = myEnvironment.size[0]/myLevel.getSize()[0]
+		# If level's y size > level's x size then resize according to y axis
+		else:
+			new_image_size = myEnvironment.size[1]/myLevel.getSize()[1]
+		
+		# Just to the resize job	
+		wall = pygame.transform.scale(wall, (new_image_size,new_image_size))
+		box = pygame.transform.scale(box, (new_image_size,new_image_size))
+		box_on_target = pygame.transform.scale(box_on_target, (new_image_size,new_image_size))
+		space = pygame.transform.scale(space, (new_image_size,new_image_size))
+		target = pygame.transform.scale(target, (new_image_size,new_image_size))
+		player = pygame.transform.scale(player, (new_image_size,new_image_size))	
+		
+	# Just a Dictionary (associative array in pyhton's lingua) to map images to characters used in level design 
+	images = {'#': wall, ' ': space, '$': box, '.': target, '@': player, '*': box_on_target}
+	
+	# Get image size. Images are always squares so it doesn't care if you get width or height
+	box_size = wall.get_width()
+	
+	# Iterate all Rows
+	for i in range (0,len(matrix_to_draw)):
+		# Iterate all columns of the row
+		for c in range (0,len(matrix_to_draw[i])):
+			myEnvironment.screen.blit(images[matrix_to_draw[i][c]], (c*box_size, i*box_size))
+
+	pygame.display.update()
+				
+def movePlayer(direction, myLevel):
+	
+	matrix = myLevel.getMatrix()
+	
+	myLevel.addToHistory(matrix)
+	
+	x = myLevel.getPlayerPosition()[0]
+	y = myLevel.getPlayerPosition()[1]
+	
+	global target_found
+	
+	#print boxes
+	#print(myLevel.getBoxes())
+	
+	if direction == "L":
+		#print("######### Moving Left #########")
+		
+		# if is_space
+		if matrix[y][x-1] == " ":
+			#print("OK Space Found")
+			matrix[y][x-1] = "@"
+			if target_found == True:
+				matrix[y][x] = "."
+				target_found = False
+			else:
+				matrix[y][x] = " "
+		
+		# if is_box
+		elif matrix[y][x-1] == "$":
+			#print("Box Found")
+			if matrix[y][x-2] == " ":
+				matrix[y][x-2] = "$"
+				matrix[y][x-1] = "@"
+				if target_found == True:
+					matrix[y][x] = "."
+					target_found = False
+				else:
+					matrix[y][x] = " "
+			elif matrix[y][x-2] == ".":
+				matrix[y][x-2] = "*"
+				matrix[y][x-1] = "@"
+				if target_found == True:
+					matrix[y][x] = "."
+					target_found = False
+				else:
+					matrix[y][x] = " "
+				
+				
+		# if is_box_on_target
+		elif matrix[y][x-1] == "*":
+			#print("Box on target Found")
+			if matrix[y][x-2] == " ":
+				matrix[y][x-2] = "$"
+				matrix[y][x-1] = "@"
+				if target_found == True:
+					matrix[y][x] = "."
+				else:
+					matrix[y][x] = " "
+				target_found = True
+				
+			elif matrix[y][x-2] == ".":
+				matrix[y][x-2] = "*"
+				matrix[y][x-1] = "@"
+				if target_found == True:
+					matrix[y][x] = "."
+				else:
+					matrix[y][x] = " "
+				target_found = True
+				
+		# if is_target
+		elif matrix[y][x-1] == ".":
+			#print("Target Found")
+			matrix[y][x-1] = "@"
+			if target_found == True:
+				matrix[y][x] = "."
+			else:
+				matrix[y][x] = " "
+			target_found = True
+		
+		# else
+		#else:
+			#print("There is a wall here")
+	
+	elif direction == "R":
+		#print("######### Moving Right #########")
+
+		# if is_space
+		if matrix[y][x+1] == " ":
+			#print("OK Space Found")
+			matrix[y][x+1] = "@"
+			if target_found == True:
+				matrix[y][x] = "."
+				target_found = False
+			else:
+				matrix[y][x] = " "
+		
+		# if is_box
+		elif matrix[y][x+1] == "$":
+			#print("Box Found")
+			if matrix[y][x+2] == " ":
+				matrix[y][x+2] = "$"
+				matrix[y][x+1] = "@"
+				if target_found == True:
+					matrix[y][x] = "."
+					target_found = False
+				else:
+					matrix[y][x] = " "
+			
+			elif matrix[y][x+2] == ".":
+				matrix[y][x+2] = "*"
+				matrix[y][x+1] = "@"
+				if target_found == True:
+					matrix[y][x] = "."
+					target_found = False
+				else:
+					matrix[y][x] = " "				
+		
+		# if is_box_on_target
+		elif matrix[y][x+1] == "*":
+			#print("Box on target Found")
+			if matrix[y][x+2] == " ":
+				matrix[y][x+2] = "$"
+				matrix[y][x+1] = "@"
+				if target_found == True:
+					matrix[y][x] = "."
+				else:
+					matrix[y][x] = " "
+				target_found = True
+				
+			elif matrix[y][x+2] == ".":
+				matrix[y][x+2] = "*"
+				matrix[y][x+1] = "@"
+				if target_found == True:
+					matrix[y][x] = "."
+				else:
+					matrix[y][x] = " "
+				target_found = True
+			
+		# if is_target
+		elif matrix[y][x+1] == ".":
+			#print("Target Found")
+			matrix[y][x+1] = "@"
+			if target_found == True:
+				matrix[y][x] = "."
+			else:
+				matrix[y][x] = " "
+			target_found = True
+			
+		# else
+		#else:
+		#	print("There is a wall here")		
+
+	elif direction == "D":
+		#print("######### Moving Down #########")
+
+		# if is_space
+		if matrix[y+1][x] == " ":
+			#print("OK Space Found")
+			matrix[y+1][x] = "@"
+			if target_found == True:
+				matrix[y][x] = "."
+				target_found = False
+			else:
+				matrix[y][x] = " "
+		
+		# if is_box
+		elif matrix[y+1][x] == "$":
+			#print("Box Found")
+			if matrix[y+2][x] == " ":
+				matrix[y+2][x] = "$"
+				matrix[y+1][x] = "@"
+				if target_found == True:
+					matrix[y][x] = "."
+					target_found = False
+				else:
+					matrix[y][x] = " "
+			
+			elif matrix[y+2][x] == ".":
+				matrix[y+2][x] = "*"
+				matrix[y+1][x] = "@"
+				if target_found == True:
+					matrix[y][x] = "."
+					target_found = False
+				else:
+					matrix[y][x] = " "
+		
+		# if is_box_on_target
+		elif matrix[y+1][x] == "*":
+			#print("Box on target Found")
+			if matrix[y+2][x] == " ":
+				matrix[y+2][x] = "$"
+				matrix[y+1][x] = "@"
+				if target_found == True:
+					matrix[y][x] = "."
+				else:
+					matrix[y][x] = " "
+				target_found = True
+				
+			elif matrix[y+2][x] == ".":
+				matrix[y+2][x] = "*"
+				matrix[y+1][x] = "@"
+				if target_found == True:
+					matrix[y][x] = "."
+				else:
+					matrix[y][x] = " "
+				target_found = True
+		
+		# if is_target
+		elif matrix[y+1][x] == ".":
+			#print("Target Found")
+			matrix[y+1][x] = "@"
+			if target_found == True:
+				matrix[y][x] = "."
+			else:
+				matrix[y][x] = " "
+			target_found = True
+			
+		# else
+		#else:
+		#	print("There is a wall here")
+
+	elif direction == "U":
+		#print("######### Moving Up #########")
+
+		# if is_space
+		if matrix[y-1][x] == " ":
+			#print("OK Space Found")
+			matrix[y-1][x] = "@"
+			if target_found == True:
+				matrix[y][x] = "."
+				target_found = False
+			else:
+				matrix[y][x] = " "
+		
+		# if is_box
+		elif matrix[y-1][x] == "$":
+			#print("Box Found")
+			if matrix[y-2][x] == " ":
+				matrix[y-2][x] = "$"
+				matrix[y-1][x] = "@"
+				if target_found == True:
+					matrix[y][x] = "."
+					target_found = False
+				else:
+					matrix[y][x] = " "
+
+			elif matrix[y-2][x] == ".":
+				matrix[y-2][x] = "*"
+				matrix[y-1][x] = "@"
+				if target_found == True:
+					matrix[y][x] = "."
+					target_found = False
+				else:
+					matrix[y][x] = " "					
+					
+		# if is_box_on_target
+		elif matrix[y-1][x] == "*":
+			#print("Box on target Found")
+			if matrix[y-2][x] == " ":
+				matrix[y-2][x] = "$"
+				matrix[y-1][x] = "@"
+				if target_found == True:
+					matrix[y][x] = "."
+				else:
+					matrix[y][x] = " "
+				target_found = True
+				
+			elif matrix[y-2][x] == ".":
+				matrix[y-2][x] = "*"
+				matrix[y-1][x] = "@"
+				if target_found == True:
+					matrix[y][x] = "."
+				else:
+					matrix[y][x] = " "
+				target_found = True
+					
+		# if is_target
+		elif matrix[y-1][x] == ".":
+			#print("Target Found")
+			matrix[y-1][x] = "@"
+			if target_found == True:
+				matrix[y][x] = "."
+			else:
+				matrix[y][x] = " "
+			target_found = True
+			
+		# else
+		#else:
+		#	print("There is a wall here")
+	
+	drawLevel(matrix)
+	
+	#print("Boxes remaining: " + str(len(myLevel.getBoxes())))
+	
+	if len(myLevel.getBoxes()) == 0:
+		myEnvironment.screen.fill((0, 0, 0))
+		print("Level Completed")
+		# global current_level
+		# current_level += 1
+		# initLevel(level_set,current_level)	
+		
+def initLevel(level_set,level):
+	# Create an instance of this Level
+	global myLevel
+	myLevel = Level(level_set,level)
+
+	# Draw this level
+	drawLevel(myLevel.getMatrix())
+	
+	global target_found
+	target_found = False
+	
+
+# Create the environment
+myEnvironment = Environment()
+
+# Choose a theme
+theme = "default"
+
+# Choose a level set
+level_set = "minicosmos"
+
+# Set the start Level
+current_level = 1
+
+target_found = False
 
 #--------------------------------RUN THE GAME WITH TWO INPUT FILES--------------------------------#
 run(sys.argv[1], sys.argv[2])
